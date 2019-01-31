9ef78379b1ce55c83508db8565d5d5a4c5dc050c
Tolani Oyefule <TOyefule@users.noreply.github.com>
2019-01-31 17:25:07 -0500

    Update and rename Main.java to Loops2.java

---
+++ Loops project -2/Loops2.java
@@ -0,0 +1,96 @@
+
+
+
+package com.company;
+import java.util.Scanner;
+
+/*
+ Loop Exercises x2
+Due No Due Date  Points None
+*/
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // 1. Write a program that prints the numbers 1-10.
+        System.out.println();
+
+                System.out.println("Numbers from 1 - 10");
+                for (int i = 0; i <= 10; i++){
+                    System.out.println(i);
+                }
+
+
+/*  1.) Prompt user for input, display the cube of all numbers up to that bound.
+
+      
+       
+
+*/  	System.out.println();
+        
+
+
+/*  2.) Prompt user for input, display the multiplication table for that number up to 10.
+Eg. 15:
+1 x 15 = 15
+2 x 15 = 30
+.
+.
+.
+10 x 15 = 150
+*/		System.out.println();
+
+/*  3.) Prompt user for input, output all odd numbers up to that bound and then the sum of those odd numbers.
+*/		System.out.println();
+
+/*  4.) Prompt user for input, output all even numbers up to that bound and then the sum of those even numbers.
+*/		System.out.println();
+
+/*  5.) Prompt user for input,
+*/		System.out.println();
+
+/*  6.) Write a loop to display the following:
+
+1
+12
+123
+1234
+12345
+*/		System.out.println();
+
+/*  7.) Write a loop to display the following:
+1
+22
+333
+4444
+55555
+*/		System.out.println();
+
+/*  8.) Write a program to display the following:
+1
+2 3
+4 5 6
+7 8 9 10
+*/		System.out.println();
+
+/*  9.) Write a program to display the following:
+1 
+01
+101 
+0101 
+10101
+*/		System.out.println();
+
+/*  10.) Prompt user for input, determine if that number is a palindrome (a number that reads the same forward and backwards) Eg. 121
+121 is a palindrome.
+*/		System.out.println();
+
+/*  11.) Write a program to sum all the integers between 100 and 200 that is divisible by 9.
+ */		System.out.println();
+
+
+
+
+    }
+}
